957799d2de283f1a1883fda052e33ba481172715
GFsilva13 <genivaldo13@gmail.com>
2025-05-17 21:45:20 -0300

    add DesafioControleFluxo

---
+++ DesafioControleFluxo/src/Exception/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class ParametrosInvalidosException extends Exception {
+	
+	
+
+}
---
+++ Sistema_Artigos/src/Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+
+    public static void main (String args[]){
+        int i = 2;
+        if (i !=1  && i !=2){
+            System.out.println("Valor = "+i);
+        }
+    }
+}
